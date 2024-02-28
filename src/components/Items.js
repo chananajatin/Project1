cdb39035fe99cfeb8b68549ef0c7c17c2bf3423f
Jatin <jatinchanana001@gmail.com>
2024-02-28 13:36:53 +0530

    css changes

--- src/components/Items.js
+++ src/components/Items.js
@@ -2,7 +2,7 @@ import Card from "./Card";
 
 const Items = () => {
   return (
-    <div className="grid grid-cols-2 h-screen sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-9 px-8 justify-items-center items-center">
+    <div className="grid grid-cols-2 px-2 h-screen sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3 xl:px-8  justify-items-center items-center ">
       <Card />
       <Card />
       <Card />
