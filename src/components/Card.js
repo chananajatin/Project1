8ac1f7e212743c543eb0c69aca05d3937f630b33
Jatin <jatinchanana001@gmail.com>
2024-02-28 22:59:47 +0530

    cart

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,56 +1,40 @@
 import React, { useState } from "react";
 import img1 from "../components/assets/2.jpg";
 import { CiShoppingCart } from "react-icons/ci"; // Import the cart icon from react-icons library
-import Modal from "./Modal"; // Import the Modal component
-import { FaTimes } from "react-icons/fa"; // Import the cross icon from react-icons library
 
 const Card = () => {
-  const [isHovered, setIsHovered] = useState(false);
   const [isAddedToCart, setIsAddedToCart] = useState(false);
 
   const handleAddToCart = () => {
     setIsAddedToCart(true);
-    setTimeout(() => setIsAddedToCart(false), 2000); // Reset added to cart message after 2 seconds
+    setTimeout(()=>{
+        setIsAddedToCart(false)
+    },2000)
   };
 
   return (
-    <div
-      className="relative overflow-hidden"
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-    >
-      <div className="w-full h-full overflow-hidden">
+    <div className="relative overflow-hidden">
+      <div className="w-full h-full overflow-hidden relative">
         <img
           src={img1}
-          className={`w-full transition-transform transform ${
-            isHovered ? "scale-105 h-3/4" : "scale-100 h-full"
-          }`}
+          className="w-full transition-transform transform hover:scale-105"
           alt="Item"
         />
+        {isAddedToCart && (
+          <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-white">
+            <p>Item Added to cart</p>
+          </div>
+        )}
       </div>
 
-      <div className="absolute bottom-0 right-0 m-4">
+      <div className="absolute bottom-0 right-0 m-1">
         <button
-          className="flex items-center justify-center  text-black  p-2 opacity-100 "
+          className="flex items-center justify-center text-black p-2 opacity-100 hover:text-gray-700"
           onClick={handleAddToCart}
         >
-          <CiShoppingCart />
+          <CiShoppingCart size={30} />
         </button>
       </div>
-
-      {isAddedToCart && (
-        <Modal onClose={() => setIsAddedToCart(false)}>
-          <div className="text-center">
-            <p className="text-xl font-bold mb-2">Added to cart</p>
-            <button
-              className="absolute top-0 right-0 m-4 text-gray-500 hover:text-gray-700"
-              onClick={() => setIsAddedToCart(false)}
-            >
-              <FaTimes />
-            </button>
-          </div>
-        </Modal>
-      )}
     </div>
   );
 };
