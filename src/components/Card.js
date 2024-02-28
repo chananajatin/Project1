86c18ad22ebb4806bd61dde0345b3db8a0666f2b
Jatin <jatinchanana001@gmail.com>
2024-02-28 16:13:02 +0530

    add to cart

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,36 @@
-import React from 'react'
+import React, { useState } from "react";
 import img1 from "../components/assets/2.jpg";
+
 const Card = () => {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
-    <>
-      <div className="relative w-full h-80 m-2 overflow-hidden">
+    <div
+      className="relative overflow-hidden "
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <div className="w-full h-full overflow-hidden">
         <img
           src={img1}
-          className="w-full h-full object-cover transition-transform transform hover:scale-105 cursor-pointer"
+          className={`w-full transition-transform transform ${
+            isHovered ? "scale-105 h-3/4" : "scale-100 h-full"
+          }`}
           alt="Item"
         />
       </div>
-    </>
-  );
-}
 
-export default Card
+      <div className="bottom-0 left-0 right-0 m-1 p-1 ">
+        <button
+          className={`p-1 transition-opacity ${
+            isHovered ? "opacity-100" : "opacity-0"
+          }`}
+        >
+          Add to cart
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
