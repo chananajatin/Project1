5b52e384b308c80d0fbe8de0395bcba647bb9d20
Jatin <jatinchanana001@gmail.com>
2024-02-29 18:01:43 +0530

    29-02-2024

--- src/components/Card2.js
+++ src/components/Card2.js
@@ -42,7 +42,7 @@ const Card2 = ({ image, name }) => {
         )}
         {wishListMessage && (
           <div className="absolute top-0 left-0 right-0 bottom-0 bg-black bg-opacity-40 flex justify-center items-center">
-            <p className="text-white font-bold">Added to WishList!❤️</p>
+            <p className="text-white font-bold">Added to WishList!</p>
           </div>
         )}
       </div>
